6d6830ad29fa2977ef12fdc3d5da112736c84871
Navin Udiaver <navinudiaver@navins-mbp.attlocal.net>
2016-11-06 00:22:05 -0500

    routes tested on postman

--- server.js
+++ server.js
@@ -36,6 +36,55 @@ app.get('/', function(req, res){
 });
 
 
+// Route to get saved articles
+app.get('/api/saved', function(req, res){
+
+	Article.find({}).exec(function(err, doc){
+		if (err) {
+			console.log(err)
+		}
+		else {
+			res.send(doc);
+		}
+	})
+});
+
+
+// Route to post saved article 
+app.post('/api/saved', function(req, res){
+	var newArticle = new Article(req.body);
+
+	console.log(req.body)
+
+	var title = req.body.title;
+	var date = req.body.date;
+	var url = req.body.url;
+
+	newArticle.save(function(err, doc){
+		if (err) {
+			console.log(err)
+		}
+		else {
+			res.send(doc._id)
+		}
+	})
+})
+
+
+// Route to delete saved article
+app.delete('/api/saved/', function(req, res){
+
+	Article.find({"url": req.param('url')}).remove().exec(function(err, data){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.send('Article deleted from saved list.');
+		}
+	})
+})
+
+
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
 })
